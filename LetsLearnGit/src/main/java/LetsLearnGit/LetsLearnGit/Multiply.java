4c3f8ff9c3583a589f96aa9724a79b7ba60a134b
Sandip Bankar <bankar.sandipk@yahoo.com>
2024-01-07 15:56:15 +0530

    Added new file 'Multiply.java'

---
+++ LetsLearnGit/src/main/java/LetsLearnGit/LetsLearnGit/Multiply.java
@@ -0,0 +1,11 @@
+package LetsLearnGit.LetsLearnGit;
+
+import java.nio.file.spi.FileSystemProvider;
+
+public class Multiply {
+public static void main(String[] args) {
+	int a =2;
+	int b = 5;
+	System.out.println(a*b);
+}
+}
