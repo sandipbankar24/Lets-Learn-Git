0de82b12334834cabb59bbaffc41ca4a913ac8b2
Sandip Bankar <bankar.sandipk@yahoo.com>
2024-01-07 15:19:11 +0530

    added new file GitDemo.java

---
+++ LetsLearnGit/src/main/java/LetsLearnGit/LetsLearnGit/GitDemo.java
@@ -0,0 +1,13 @@
+package LetsLearnGit.LetsLearnGit;
+
+public class GitDemo {
+	
+	static int addition(int a, int b){
+		
+		return a +b;
+	}
+	public static void main(String[] args) {
+		
+		System.out.println(addition(5,6));
+	}
+}
