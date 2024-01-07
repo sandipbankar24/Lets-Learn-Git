15ddf1e3d08aa258310e95834a6f6eb2ae765f2f
Sandip Bankar <bankar.sandipk@yahoo.com>
2024-01-07 15:21:27 +0530

    changes done to GitDemo.java

--- LetsLearnGit/src/main/java/LetsLearnGit/LetsLearnGit/GitDemo.java
+++ LetsLearnGit/src/main/java/LetsLearnGit/LetsLearnGit/GitDemo.java
@@ -2,10 +2,11 @@ package LetsLearnGit.LetsLearnGit;
 
 public class GitDemo {
 	
-	static int addition(int a, int b){
+	static int addition(int a, int b){//simple addition method
 		
 		return a +b;
 	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println(addition(5,6));
